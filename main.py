1c969788518ac4ce4715a707ab2a1241f96c170c
peledemilhoesoficial26-pixel <peledemilhoesoficial26@gmail.com>
2026-06-29 01:13:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+import telebot
+
+TOKEN = "8751717795:AAFtz9g0xrUIDYvVhRMCUA9JWEHB3pjsGIo"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Olá! O robô Pele de Milhões está online e pronto para trabalhar!")
+
+print("Robô iniciado com sucesso...")
+bot.infinity_polling()
