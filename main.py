f69a51228ec4d665f4e717f863d939018370173f
peledemilhoesoficial26-pixel <peledemilhoesoficial26@gmail.com>
2026-06-29 02:22:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import telebot
+from threading import Thread
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
+# Cria um servidor web falso para a Render não dar Timed Out
+class FakeServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Robo Online")
+
+def run_web_server():
+    server = HTTPServer(('0.0.0.0', 10000), FakeServer)
+    server.serve_forever()
+
+# Inicia o servidor falso em segundo plano
+Thread(target=run_web_server, daemon=True).start()
+
+# Seu robô oficial
 TOKEN = "8751717795:AAHtRIJnEEOhKArso18kRfpBhb48BwUg4Ls"
 bot = telebot.TeleBot(TOKEN)
 
